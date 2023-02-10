989663d46224ad641171657364dd4864b14e144b
kura77 <115204937+kurana0b@users.noreply.github.com>
2023-02-10 12:13:14 +0900

    added file

---
+++ src/main/java/jp/thirtycraft/loginbonus/Loginbonus.java
@@ -0,0 +1,17 @@
+package jp.thirtycraft.loginbonus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Loginbonus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
