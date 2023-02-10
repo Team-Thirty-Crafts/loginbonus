4ff0c94e5b5a12eceaf4c6cd868e771ffa39f5d0
kura77 <115204937+kurana0b@users.noreply.github.com>
2023-02-10 13:00:13 +0900

    Update LoginBonus.java

--- src/main/java/jp/thirtycraft/loginbonus/events/LoginBonus.java
+++ src/main/java/jp/thirtycraft/loginbonus/events/LoginBonus.java
@@ -1,5 +1,9 @@
 package jp.thirtycraft.loginbonus.events;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,10 +12,19 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoginBonus extends JavaPlugin implements Listener {
+    private Map<Player, Date> lastLoginTime = new HashMap<>();
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage(ChatColor.GREEN + "サーバーへようこそ! ログインボーナスはこちらです。50枚のコインをプレゼント!");
-        // ボーナスをプレイヤーのゲーム内通貨に加算する（該当する場合）。
+        Date now = new Date();
+        Date last = lastLoginTime.get(player);
+
+        if (last == null || now.getTime() - last.getTime() >= 24 * 60 * 60 * 1000) {
+            player.sendMessage(ChatColor.GREEN + "サーバーへようこそ! ログインボーナスはこちらです。50枚のコインをプレゼント!");
+            // ボーナスをプレイヤーのゲーム内通貨に加算する（該当する場合）。
+        }
+
+        lastLoginTime.put(player, now);
     }
 }
