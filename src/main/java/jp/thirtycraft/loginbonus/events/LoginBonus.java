094c8d6363b9be873250be4c90f271edacdd121a
kura77 <115204937+kurana0b@users.noreply.github.com>
2023-02-12 22:16:31 +0900

    Update LoginBonus.java

--- src/main/java/jp/thirtycraft/loginbonus/events/LoginBonus.java
+++ src/main/java/jp/thirtycraft/loginbonus/events/LoginBonus.java
@@ -1,18 +1,23 @@
 package jp.thirtycraft.loginbonus.events;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ItemStack;
 
-public class LoginBonus extends JavaPlugin implements Listener {
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class LoginBonus implements Listener {
     private Map<Player, Date> lastLoginTime = new HashMap<>();
+    private Material[] materials = {
+
+    };
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -20,11 +25,18 @@ public class LoginBonus extends JavaPlugin implements Listener {
         Date now = new Date();
         Date last = lastLoginTime.get(player);
 
-        if (last == null || now.getTime() - last.getTime() >= 24 * 60 * 60 * 1000) {
-            player.sendMessage(ChatColor.GREEN + "サーバーへようこそ! ログインボーナスはこちらです。50枚のコインをプレゼント!");
-            // ボーナスをプレイヤーのゲーム内通貨に加算する（該当する場合）。
+        int daysSinceLastLogin = (int) ((now.getTime() - last.getTime()) / (24 * 60 * 60 * 1000));
+
+        if (last == null || daysSinceLastLogin >= 1) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(materials.length);
+            Material randomMaterial = materials[randomIndex];
+
+            player.sendMessage(ChatColor.GREEN + "ログインボーナスで" + randomMaterial + "をプレゼント!");
+            player.getInventory().addItem(new ItemStack(randomMaterial, 1));
         }
 
         lastLoginTime.put(player, now);
     }
 }
+
