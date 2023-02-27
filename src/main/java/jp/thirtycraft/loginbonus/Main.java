76948cd622594859d3783e2b89285e8ee9e47102
kura77 <115204937+kurana0b@users.noreply.github.com>
2023-02-27 15:39:20 +0900

    Update Main.java

--- src/main/java/jp/thirtycraft/loginbonus/Main.java
+++ src/main/java/jp/thirtycraft/loginbonus/Main.java
@@ -3,13 +3,18 @@ package jp.thirtycraft.loginbonus;
 import jp.thirtycraft.loginbonus.events.LoginBonus;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class Main extends JavaPlugin {
 
     JavaPlugin plugin = this;
+    Logger logger = Logger.getLogger("lb");
+
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new LoginBonus(), this);
+        logger.info("login bonusが正常に起動しました");
     }
 
     @Override
